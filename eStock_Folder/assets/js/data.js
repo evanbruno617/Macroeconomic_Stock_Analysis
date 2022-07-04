e238e9f816c1f1d7afe652e86d50e081187aca60
Evan Bruno <evanbruno@Evans-MacBook-Air.local>
2022-07-04 11:10:01 -0700

    Graphs of financial data

---
+++ eStock_Folder/assets/js/data.js
@@ -0,0 +1,13 @@
+import industrial from '../json/Industrial.json' assert {type: 'json'};
+
+import inflation from '../json/inflation.json' assert {type: 'json'};
+
+import retail from '../json/Retail_Sales.json' assert {type: 'json'};
+
+import unemployment from '../json/Unemployment.json' assert {type: 'json'};
+
+// import interest from '../json/Industrial.json' assert {type: 'json'};
+
+
+
+
